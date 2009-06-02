ae3968fba491bcbeebda6404613abe8b1ec8960c
kakikubo <kakikubo@cf2651cb-8824-0410-88cd-b1deea40b454>
2009-06-02 14:32:09 +0000

    実行結果まで貼付けてしまっていた。
    git-svn-id: file:///Users/kakikubo/SVHack/trunk/Hack/RubyCookBook@997 cf2651cb-8824-0410-88cd-b1deea40b454

--- 1/5timecalc.rb
+++ 1/5timecalc.rb
@@ -59,33 +59,3 @@ p (leap_year_day << 1).to_s # !> (...) interpreted as grouped expression
 p (leap_year_day >> 1).to_s # !> (...) interpreted as grouped expression
 p (leap_year_day >> 12).to_s # !> (...) interpreted as grouped expression
 p (leap_year_day << 12 * 4).to_s # !> (...) interpreted as grouped expression
-# >> Sat Jan 01 00:00:01 UTC 2000
-# >> Fri Dec 31 23:59:59 UTC 1999
-# >> Sun Dec 31 00:00:00 UTC 2000
-# >> "2000-01-02T00:00:00+00:00"
-# >> "1999-12-31T00:00:00+00:00"
-# >> "2000-01-01T12:00:00+00:00"
-# >> "2000-12-31T00:00:00+00:00"
-# >> 86400.0
-# >> -86400.0
-# >> Rational(1, 1)
-# >> Rational(-1, 1)
-# >> 1.000377
-# >> Rational(37053, 3200000000)
-# >> Sat May 23 22:14:46 +0900 2009
-# >> Thu Jul 02 22:14:46 +0900 2009
-# >> Sun Jan 15 00:00:00 +0900 2006
-# >> Fri Dec 31 00:00:00 UTC 1999
-# >> Sat Apr 01 00:00:00 UTC 2006
-# >> Sat Apr 01 00:00:00 UTC 2006
-# >> "2000-02-01T00:00:00+00:00"
-# >> "1999-12-01T00:00:00+00:00"
-# >> Tue, 01 Feb 2000 00:00:00 +0000
-# >> Wed, 01 Dec 1999 00:00:00 +0000
-# >> "2000-09-30"
-# >> "2000-11-30"
-# >> "2000-12-31"
-# >> "1996-01-29"
-# >> "1996-03-29"
-# >> "1997-02-28"
-# >> "1992-02-29"
