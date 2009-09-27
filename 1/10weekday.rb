d3a8d77e866eaa811a681bc994a99811444eddd0
kakikubo <kakikubo@cf2651cb-8824-0410-88cd-b1deea40b454>
2009-09-27 14:12:05 +0000

    refs #368
    git-svn-id: file:///Users/kakikubo/SVHack/trunk/Hack/RubyCookBook@1029 cf2651cb-8824-0410-88cd-b1deea40b454

--- 1/10weekday.rb
+++ 1/10weekday.rb
@@ -15,4 +15,32 @@ def print_every_sunday(d1, d2)
   every_sunday(d1, d2) { |sunday| puts sunday.strftime("%x")}
 end
 
-print_every_sunday(Time.local(2006, 1, 1), Time.local(2006, 2, 4))
+print_every_sunday(Time.local(2009, 9, 1), Time.local(2009, 9, 30))
+
+# 1年の何週目の週かを表すメソッド week
+module Week
+  def week
+    (yday + 7 - wday) / 7
+  end
+end
+class Date
+  include Week
+end
+
+class Time
+  include Week
+end
+
+saturday = DateTime.new(2009, 1, 3)
+p saturday.week
+p (saturday + 1).week # !> (...) interpreted as grouped expression
+p (saturday - 1).week # !> (...) interpreted as grouped expression
+
+
+# >> 09/06/09
+# >> 09/13/09
+# >> 09/20/09
+# >> 09/27/09
+# >> 0 #当日は一週目(0)
+# >> 1 #次の日は２週目
+# >> 0 #前日は１週め(0)
